5fe8d81c7cb1659183fa7ed1071c962ab233aa52
dhar-rgb <genedharma@gmail.com>
2024-07-20 10:48:59 -0500

    Initial commit

---
+++ Selenium/src/week1/day2/isPrime.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public static boolean isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
