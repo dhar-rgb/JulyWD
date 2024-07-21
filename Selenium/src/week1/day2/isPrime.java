836d9ffe81251c747af6c222f200976e467d11a0
dhar-rgb <83975045+dhar-rgb@users.noreply.github.com>
2024-07-21 07:59:25 -0500

    Add files via upload

--- Selenium/src/week1/day2/isPrime.java
+++ Selenium/src/week1/day2/isPrime.java
@@ -1,15 +1,29 @@
+
 package week1.day2;
 
-public static boolean isPrime(int number) {
-    if (number <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= number; i++) {
-        if (number % i == 0) {
-            return false;
+public class isPrime {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 4, 5, 6};
+        
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                System.out.println("Number " + number + " is prime.");
+            } else {
+                System.out.println("Number " + number + " is not prime.");
+            }
         }
     }
-    return true;
-}
 
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
